f63efc5a9f840857970f6befbd200d584cab5907
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-12-03 13:32:10 +0200

    updated recks package

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.scss';
 function App () {
     return <div class="app">
         <div class="app-header">
-            <h1>Recks 🐶</h1>
+            <h1>🐶 Recks</h1>
             <h2>is ready to serve</h2>
             <h3><a href="https://github.com/recksjs/recks" target="_blank">[github]</a></h3>
         </div>
